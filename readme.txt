dbac9f835e2f73acb932c42d092985f829c4a1d5
melbaa <none@no-reply>
2023-05-10 14:24:48 +0300

    ye

--- readme.txt
+++ readme.txt
@@ -1,2 +1,9 @@
+/sundercounts
+prints how many sunders people did
+
+/sunderreset
+resets the counts you have collected
+
+
 related work
 https://github.com/Horiwix/Sunderstruck
--- sunderarmor.lua
+++ sunderarmor.lua
@@ -1,7 +1,29 @@
-
 local function print(msg)
     DEFAULT_CHAT_FRAME:AddMessage(msg)
 end
+
+
+
+
+local frame = CreateFrame("Frame")
+local addon_prefix_sunder_cast = 'SACAST'
+
+local sundercounts = {}
+
+frame:RegisterEvent("CHAT_MSG_ADDON")
+frame:SetScript("OnEvent", function()
+    if event ~= 'CHAT_MSG_ADDON' then return end
+    if arg1 ~= 'SACAST' then return end
+
+    if not sundercounts[arg4] then
+        sundercounts[arg4] = 0
+    end
+
+    sundercounts[arg4] = sundercounts[arg4] + 1
+end)
+
+
+
 
 local spellidcache = {}
 
@@ -10,7 +32,7 @@ local function GetSpellCooldownByName(spellName)
         -- check cache first
         local spellid = spellidcache[spellName]
         if spellid then
-            print('found in cache')
+            -- print('found in cache')
             local _, duration = GetSpellCooldown(spellid, bookType);
             return duration;
         end
@@ -25,7 +47,7 @@ local function GetSpellCooldownByName(spellName)
             end
             
             if name == spellName then
-                print(string.format('name %s rank %s', name, spellRank))
+                -- print(string.format('name %s rank %s', name, spellRank))
                 spellidcache[name] = i
                 local _, duration = GetSpellCooldown(i, bookType);
                 return duration;
@@ -73,7 +95,6 @@ function hooksecurefunc(name, func, append)
 end
 
 
-local sundercount = 0
 local lastsunder = 0
 
 hooksecurefunc("CastSpellByName", function(spell, target)
@@ -83,34 +104,60 @@ hooksecurefunc("CastSpellByName", function(spell, target)
     --print(string.format('testing %s %s', tostring(spell), tostring(target)))
     spell = string.lower(spell)
     if not string.find(spell, 'sunder armor') then
-        print('spell name?')
+        -- print('spell name?')
         return
     end
 
     -- Now we've found sunder. Check the cooldown
     local cd = GetSpellCooldownByName('Sunder Armor')
     if cd == 0 then
-        print(string.format('cooldown? %s ', cd))
+        -- print(string.format('cooldown? %s ', cd))
         return
     end
 
     -- Test for target
     if UnitCanAttack("player", "target") == nil then
-        print('cannot attack?')
+        -- print('cannot attack?')
         return
     end
 
     local now = GetTime()
     if lastsunder + cd >= now then
-        print('cooldown 2?')
+        -- print('cooldown 2?')
         return
     end
 
     lastsunder = GetTime()
 
-    sundercount = sundercount + 1
-    print(string.format('sunder count %s', sundercount))
+    SendAddonMessage(addon_prefix_sunder_cast, 'whatever', "RAID")
+    print('sundered!')
 end, true)
 
 
+local function dumpcounts()
+    print('sunder counts:')
 
+    local len = 0
+    for k, v in pairs(sundercounts) do
+        print(string.format('%s %s', k, v))
+        len = len + 1
+    end
+
+    if len == 0 then
+        print('no data')
+    end
+
+    print('sunder counts end')
+end
+
+local function resetcounts()
+    sundercounts = {}
+    print('sunder counts reset')
+end
+
+
+SLASH_SUNDERCOUNTS1 = "/sundercounts";
+SlashCmdList["SUNDERCOUNTS"] = dumpcounts
+
+SLASH_SUNDERRESET1 = "/sunderreset";
+SlashCmdList["SUNDERRESET"] = resetcounts
